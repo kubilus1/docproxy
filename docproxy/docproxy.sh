a38d08754695fe290e40bff0bb8299bd6b566b63
Matt Kubilus <mattkubilus@gmail.com>
2020-03-20 09:13:00 -0400

    Setup a default

--- docproxy/docproxy.sh
+++ docproxy/docproxy.sh
@@ -3,9 +3,10 @@
 buildconf() {
     echo "Rebuilding conf"
     FRONT=$(docker ps --format '\n   acl {{.Names}}-api  hdr_beg(host) -i {{.Label "subdomain"}}.\n   use_backend {{.Names}}-backend if {{.Names}}-api\n' --filter "label=api")
+    DEFAULT=$(docker ps --format '\n   default_backend {{.Names}}-backend\n' --filter "label=api" --filter "label=default")
     BACK=$(docker ps --format 'backend {{.Names}}-backend\n   server {{.Label "subdomain" }}-server {{.Names}}:{{.Label "port" }}\n\n' --filter "label=api")
 
-    FRONT="$FRONT" BACK="$BACK" /docproxy/docproxy.tmpl > /etc/haproxy/haproxy.cfg
+    FRONT="$FRONT" DEFAULT="$DEFAULT" BACK="$BACK" /docproxy/docproxy.tmpl > /etc/haproxy/haproxy.cfg
     service haproxy reload
 }
 
